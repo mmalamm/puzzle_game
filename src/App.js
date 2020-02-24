1d724ddc86dcdab30a532f2fe196952373b79536
M M Alam <alam@mmtutor.com>
2020-02-24 00:56:43 -0500

    make size responsive

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import classes from "./App.module.css";
+import useWindowSize from "./useWindowSize";
 import {
   isCellClickable,
   createInitialAppState,
@@ -10,6 +11,7 @@ import {
 
 export default function App() {
   const [state, setState] = useState(createInitialAppState());
+  const size = useWindowSize();
   useEffect(() => {
     if (isGameWon(state.grid)) {
       alert("You win :)");
@@ -23,6 +25,8 @@ export default function App() {
     const newState = createNewState(clickedCell, state);
     setState(newState);
   };
+
+  const cellSize = size.width > 800 ? 200 : size.width / 4;
 
   return (
     <div className={classes.app}>
@@ -40,9 +44,11 @@ export default function App() {
                 {...interactionProp}
                 style={{
                   backgroundImage: `url("https://avatars2.githubusercontent.com/u/23639980?s=400&v=4")`,
-                  backgroundSize: "400px",
+                  backgroundSize: cellSize * 4 + "px",
                   backgroundRepeat: "no-repeat",
-                  backgroundPosition: getBgPos(c)
+                  backgroundPosition: getBgPos(c, cellSize),
+                  width: cellSize + "px",
+                  height: cellSize + "px"
                 }}
               ></p>
             );
